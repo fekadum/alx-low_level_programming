37aa94fc238c4211e61fa0cfe801cc8f10bf075e
fekadu <fekadum662@gmail.com>
2022-12-08 13:26:32 -0800

    adfasdf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+
 /**
- * main - Entry
- * Retrun: Alwasys 0
- */
+ *
+ *  * main - Entry point
+ *
+ *   * Description: 'the program's description'
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
-{ 
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
