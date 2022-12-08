0b748f35da2df7b0909da49b401be9145b7c15e7
fekadu <fekadum662@gmail.com>
2022-12-08 14:17:31 -0800

    adfadf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
 
-
-
 /**
- *
- * *main -Entry point
+ * main -Entry point
  *
  * *Return: Always 0 (Success)
  *
- * */
-
-
-
+ **/
 int main(void)
-
 {
-
 		printf("Size of a char: %zu byte(s)\n", sizeof(char));
 
 			printf("Size of an int: %zu byte(s)\n", sizeof(int));
