47b8aa129676f66f61660020291f7c8f2c936c50
fekadu <fekadum662@gmail.com>
2022-12-09 04:02:08 -0800

    adfadf

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main -function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+
+		int n;
+
+
+
+			srand(time(0));
+
+				n = rand() - RAND_MAX /
+				if (n == 0)
+				{
+				printf("%d: " + n "is zero")
+				}
+				else if (n  > 0)
+				{
+				printf("%d:" + n + "is posetive")
+				}
+				else
+				{
+				printf("%d:" + n + "is negative")
+				}
+
+					return (0);
+
+}
