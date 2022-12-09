6391fc2e07fd16ebe3b41026a690a6729931c1ae
fekadu <fekadum662@gmail.com>
2022-12-09 04:40:48 -0800

    commit all

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX /
 
-if (n == 0)
-	printf("6%d is zero\n", n)
-else if (n  > 0)
-	printf("%d is posetive\n", n)
+if (n >  0)
+	printf("%d is positive\n", n)
+else if (n == 0)
+	printf("%d is zero\n", n)
 else
 	printf("%d is negative\n", n)
 return (0);
