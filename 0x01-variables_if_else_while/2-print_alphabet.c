0ac7017a1e04efaab4842c243298c176519cc3a6
fekadu <fekadum662@gmail.com>
2022-12-09 12:13:45 -0800

    adf

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,24 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
- * main - Entry point
- *This program will assign a random number to the variable n
- * Return: Always 0 (Success)
- */
+*main - Printthe alphabetic
+* a program that prints the alphabet in lowercase, followed by a new line.
+*Return: Always (success)
+*/
 int main(void)
 {
-	int n;
+char c;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if (n % 10 > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
-	else if (n % 10 == 0)
-		printf("Last digit of %d is %d and is 0\n", n, n % 10);
-	else
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
-
-	return (0);
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
 }
