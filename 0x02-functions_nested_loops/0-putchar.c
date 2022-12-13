2819c1c4f5c7349cb9a482b00e9dbedacc3ec367
fekadu <fekadum662@gmail.com>
2022-12-13 05:56:38 -0800

    adf

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,24 @@
 #include "main.h"
 /**
- * main - Entry Block
-  *Description: prints _putchar followed by a new line
- * Return: 0
- */
+ *
+ * * main - print _putchar
+ *
+ * * is written by mbah
+ *
+ * * Return: 0
+ *
+ * */
+
 int main(void)
 {
-	char c[8] = "_putchar";
-
-	int i = 0;
-
-	while (i < 8)
-	{
-		_putchar(c[i]);
-		i++;
-	}
-	_putchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
