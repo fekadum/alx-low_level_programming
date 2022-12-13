5702ac18db3e8553beb26549f29ffea83861f48a
fekadu <fekadum662@gmail.com>
2022-12-13 11:31:49 -0800

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,6 +8,7 @@
 void print_to_98(int n)
 {
 int i, j;
+
 if (n <= 98)
 {
 for (i = n; i <= 98; i++)
@@ -19,7 +20,6 @@ printf("%d\n", i);
 }
 }
 else if (n >= 98)
-iiiii
 {
 for (j = n; j >= 98; j--)
 {
