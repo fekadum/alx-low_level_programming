4b31e8444899fd27ba50f33f4dc6287a0f31ed92
fekadu <fekadum662@gmail.com>
2023-01-02 06:08:27 -0800

    bonus

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,4 +1,13 @@
-#include"main.h"
-void set_string(char **s, char *to){
-s=*to;
+#include "main.h"
+
+/**
+  * set_string - Sets the value of a pointer to char
+  * @s: the value to modify
+  * @to: the value to assign
+  *
+  * Return: Nothing.
+  */
+void set_string(char **s, char *to)
+{
+	*s = to;
 }
